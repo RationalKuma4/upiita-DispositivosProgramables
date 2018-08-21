686a9b1c837950a22268f100275a30ac3bcde1b8
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-08-21 10:36:04 -0500

    Practica

---
+++ DSP0001_codec_efectos/Debug/subdir_vars.mk
@@ -0,0 +1,49 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../C6713dsk.cmd 
+
+ASM_SRCS += \
+../Vectors_intr.asm 
+
+C_SRCS += \
+../c6713dskinit.c \
+../main.c 
+
+C_DEPS += \
+./c6713dskinit.d \
+./main.d 
+
+OBJS += \
+./Vectors_intr.obj \
+./c6713dskinit.obj \
+./main.obj 
+
+ASM_DEPS += \
+./Vectors_intr.d 
+
+OBJS__QUOTED += \
+"Vectors_intr.obj" \
+"c6713dskinit.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"c6713dskinit.d" \
+"main.d" 
+
+ASM_DEPS__QUOTED += \
+"Vectors_intr.d" 
+
+ASM_SRCS__QUOTED += \
+"../Vectors_intr.asm" 
+
+C_SRCS__QUOTED += \
+"../c6713dskinit.c" \
+"../main.c" 
+
+
