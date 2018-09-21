529633a90d178a2aae863afe5b20b286b0641ebf
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-09-21 11:26:25 -0500

    Filtraje en cascada
    Solo faltan graficas

--- FiltrosMuestra/FiltroIirCascada.m
+++ FiltrosMuestra/FiltroIirCascada.m
@@ -10,7 +10,7 @@ a=dlmread('cascada1.txt');
 
 %% Iniciamos el filtraje IIR por muestra
 [inputSample, Fs]=audioread('elpasofs16000Nb16_3.wav');
-inputSample=inputSample(1:10);
+%inputSample=inputSample(1:10);
 Na=length(a);
 Nx=length(inputSample);
 yy=filter(b,a,inputSample);
@@ -21,7 +21,6 @@ for n=1:Nx
     tempa=0;
     tempb=0;
     
-    y=0;
     for k=2:Na
         tempa=tempa-a(k)*u(k);
         tempb=tempb+b(k)*u(k);
@@ -34,9 +33,12 @@ for n=1:Nx
     for m=Na:-1:2
         u(m)=u(m-1);
     end
+%     yy
+%     ye
+%     pause
 end
 % Prueba de audio
-%sound(yv(1:160000), Fs);
+sound(yv(1:160000), Fs);
 
 %% Escrbimos y leemos los matriz y ganancia
 %dlmwrite('ganancia.txt', G, 'delimiter', ',', 'precision', 30); 
@@ -46,39 +48,36 @@ SOS=dlmread('matriz.txt');
 
 %% Iniciamos el filtraje IIR en cascada
 disp('Filtraje en cascada')
-Ne=2;
+Ne=4;
 gg=G';
 bb=SOS(1:4,1:3);
 aa=SOS(1:4,4:6);
 uu=zeros(Ne,3);
-yv=zeros(Ne,3);
+yv=zeros(1,Na);
 
 for n=1:Nx
-    x=inputSample(n);
+    x(1)=inputSample(n);
     for e=1:Ne
         temp1=0;
         temp2=0;
         
         for k=2:3
             temp1=temp1-aa(e,k)*uu(e,k);
-            temp2=temp2-bb(e,k)*uu(e,k);
+            temp2=temp2+bb(e,k)*uu(e,k);
         end
         
         uu(e,1)=gg(e)*x+temp1;
         ye=uu(e,1)*bb(e,1)+temp2;
-        
+        yv(n)=ye;
         for m=3:-1:2
             uu(e,m)=uu(e,m-1);
         end
         x=ye;
     end
-    yy
-    y
-    pause
+%     yy
+%     ye
+%     pause
 end
 
 % Prueba de audio
-%sound(yv(1:160000), Fs);
-
-
-
+sound(yv(1:160000), Fs);
